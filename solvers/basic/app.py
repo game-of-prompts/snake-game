1a5b09ec36569f562cb69b9254dbe1ace77239d6
0xf965 <40121100+0xf965@users.noreply.github.com>
2026-02-23 18:09:14 +0100

    Update app.py

--- solvers/basic/app.py
+++ solvers/basic/app.py
@@ -75,4 +75,4 @@ def get_move():
     return jsonify({'move': move})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=False)
